7b2c8a2f5570e08b207f19b0826a8a98d6f9b455
Stian <stian.verherstraeten@student.uhasselt.be>
2024-06-18 14:40:20 +0200

    Update sidebarheight.js

--- scripts/sidebarheight.js
+++ scripts/sidebarheight.js
@@ -8,7 +8,10 @@ window.addEventListener('resize', function() {
 
 function adjustSidebarHeight() {
     var navbarHeight = document.getElementById('main-navbar').offsetHeight;
-    var windowHeight = document.getElementById('main-content').offsetHeight;
+    var body = document.body
+    var html = document.documentElement;
+
+    var windowHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
     var sidebar = document.querySelector('.sidebar');
     sidebar.style.height = (windowHeight - navbarHeight) + 'px';
 }
